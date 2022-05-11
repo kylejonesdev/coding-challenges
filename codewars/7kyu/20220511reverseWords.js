4c6edebc92cb172c35c8790510836d2725fe8c49
Kyle Jones <kyle@kylejones.dev>
2022-05-11 14:53:06 -0700

    Reverse the words in a string.

---
+++ codewars/7kyu/20220511reverseWords.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/javascript
+
+//Reverse the words in a string.
+
+function reverseWords(str) {
+    //let arr = str.split(' ').reverse();
+    return str.split(' ').map(item => item.split('').reverse().join('')).join(' ');
+}
