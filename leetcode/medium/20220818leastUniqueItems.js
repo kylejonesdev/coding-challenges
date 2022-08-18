5df3c924f434c3c40025f323db2fbeee5d2e595b
Kyle Jones <kyle@kylejones.dev>
2022-08-18 12:06:15 -0700

    Fix bugs, reduce time complexity

--- leetcode/medium/20220818leastUniqueItems.js
+++ leetcode/medium/20220818leastUniqueItems.js
@@ -3,32 +3,36 @@
  * @param {number[]} arr
  * @return {number}
  */
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
  var minSetSize = function(arr) {
     //Find out how many times each unique number occurs
     let numCount = {};
     for(let i of arr) {
         if(!numCount[i]) {
-            numCount[i] = arr.reduce((acc, item) => {
-                return item === i ? acc += 1 : acc;
-            },0)
+            numCount[i] = 1
+        } else if (numCount[i]) {
+            numCount[i] ++;
+        } else {
+            continue;
         }
     }
+    //In JS, object keys cannot be sorted, so create an array holding the number of times each unique number occurs
+    let occurancesArr = [];
+    for(let j in numCount) {
+        occurancesArr.push(numCount[j]);
+    }
+    //Sort the occurances array so that the highest number of occurances are at the beginning
+    let sortedOccurancesArr = occurancesArr.sort((a, b) => b - a);
     let tally = 0; //count of array items
-    let count = 0; //number of unique array items required to cover half of the number of array items    
+    let count = 0;//number of unique array items required to cover half of the number of array items
+    //Add up the occurances, starting with the largest, until it equals half the length of the array
     while(tally < arr.length / 2) {
-        //Find the unique number with the most occurances
-        let greatest = 0; //current occurance count        
-        for(let num in numCount) {        
-            if(numCount[num] > greatest){
-                greatest = num; 
-            }
-        }
-        //Add the number of times the chosen number occurs to the tally
-        tally += numCount[greatest];
-        //Remove the array item with the greatest number of occurances from the numCount so it isn't counted again
-        numCount[greatest] = 0;
-        //Increase the unique items needed by one
+        tally += sortedOccurancesArr[count];
         count ++;
     }
-    return count;    
+    return count;
+        
 };
