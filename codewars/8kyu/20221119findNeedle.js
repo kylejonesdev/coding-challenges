dc913d665d8cb95205f313719a28d5f6c2c8d3a4
Kyle Jones <kyle@kylejones.dev>
2022-11-19 15:23:05 -0800

    Find index of str in array

---
+++ codewars/8kyu/20221119findNeedle.js
@@ -0,0 +1,6 @@
+//Find the position of the string "needle" in an array
+
+function findNeedle(haystack) {
+    let pos = haystack.indexOf('needle');  
+    return `found the needle at position ${pos}`;
+}
