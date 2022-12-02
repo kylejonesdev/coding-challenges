3e4750b7f7f56ddd14f94ee65fe2d575d4e38e17
Kyle Jones <kyle@kylejones.dev>
2022-12-02 10:23:01 -0800

    Find non-negative total

--- codewars/8kyu/20221201reverseSign.js
+++ codewars/8kyu/20221201reverseSign.js
@@ -2,8 +2,6 @@
 //For some reason the problem tries to assign zero a positive and negative value
 function invert(array) {
     return array.map(item => {
-      if(item === 0) return -0;
-      if(item === -0) return 0;
-      return item - item * 2;
+      return -item;
     });
 }
