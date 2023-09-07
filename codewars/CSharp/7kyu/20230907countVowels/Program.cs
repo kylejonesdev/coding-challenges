ce512cae8c1f3c5245342e5ac3bb27beac919027
Kyle Jones <kyle@kylejones.dev>
2023-09-07 14:39:33 -0700

    Count vowels in a string.

---
+++ codewars/CSharp/7kyu/20230907countVowels/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _20230907countVowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("a"));
+            Console.WriteLine(GetVowelCount("aaa"));
+            Console.WriteLine(GetVowelCount("aeiou"));
+        }
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            foreach (char character in str)
+            {
+                if ("aeiou".Contains(character))
+                {
+                    vowelCount = vowelCount + 1;
+                }
+            }
+            return vowelCount;
+        }
+        //Overly complicated. C# strings can be iterated without transformation.
+        public static int GetVowelCount2(string str)
+        {
+            int vowelCount = 0;
+            char[] charArray = str.ToCharArray();
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                if ("aeiou".Contains(charArray[i]))
+                {
+                    vowelCount = vowelCount + 1;
+                }
+            }
+            return vowelCount;
+        }
+    }
+}
