0dc83118d45cad317bb745ed81c5d9e6f5ab23f8
Kyle Jones <kyle@kylejones.dev>
2023-09-08 10:57:31 -0700

    Change one char type in a string

---
+++ codewars/CSharp/8kyu/20230908DNAtoRNA/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _20230908DNAtoRNA
+{
+    internal class Program
+    {
+        public string dnaToRna(string dna)
+        {
+            string res = "";
+            foreach (char c in dna)
+            {
+                if (c == 'T') //single quotes create a char
+                {
+                    res += "U"; //double quotes create a string
+                }
+                else
+                {
+                    res += c;
+                }
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program(); //using a non-static class requires an instance of that class 
+            Console.WriteLine(myProgram.dnaToRna("GTAC"));
+        }
+    }
+}
