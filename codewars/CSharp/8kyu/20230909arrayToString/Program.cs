36295670d741bccc12016287fde30451801cce0b
Kyle Jones <kyle@kylejones.dev>
2023-09-09 09:13:18 -0700

    Combine array of strings into a single sentence.

---
+++ codewars/CSharp/8kyu/20230909arrayToString/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _20230909arrayToString
+{
+    internal class Program
+    {
+        public static string Smash(string[] words)
+        {
+            return string.Join(' ', words);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Smash(new string[] { "Hello", "Bob"}) + " should show Hello Bob");
+        }
+    }
+}
