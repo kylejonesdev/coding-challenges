d80be03f27ad95bf76f1a706e6beaab4c224f9f3
Kyle Jones <kyle@kylejones.dev>
2023-09-11 07:51:56 -0700

    Show special string on specific argument.

---
+++ codewars/CSharp/8kyu/20230911stringIf/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _20230911stringIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(greet("Johnny"));
+        }
+        public static string greet(string name)
+        {
+            if (name == "Johnny") return "Hello, my love!";
+            return "Hello, " + name + "!";
+        }
+    }
+}
