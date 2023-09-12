9a6a316c04ab48789c6412a61597425a4c43aa86
Kyle Jones <kyle@kylejones.dev>
2023-09-12 14:27:44 -0700

    Interpolate repeated string to limit.

---
+++ codewars/CSharp/8kyu/20230912countSheep/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _20230912countSheep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSheep(3));
+        }
+        public static string CountSheep(int n)
+        {
+            string res = "";
+            for(int i = 1; i <= n; i++)
+            {
+                res += $"{i} sheep...";
+            }
+            return res;
+        }
+    }
+}
