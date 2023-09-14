acd38f82b7550b5789fdad710c2e82fefd376ea7
Kyle Jones <kyle@kylejones.dev>
2023-09-14 14:04:12 -0700

    Find lowest and highest value in array.

---
+++ codewars/CSharp/8kyu/20230913maxMinOfList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _20230914maxMinOfList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 0, 5, 4, 1, 3, 2 };
+            Program program = new Program();
+
+            Console.WriteLine(program.Min(list));
+            Console.WriteLine(program.Max(list));
+        }
+        public int Min(int[] list)
+        {
+            System.Array.Sort(list);
+            return list[0];
+        }
+
+        public int Max(int[] list)
+        {
+            System.Array.Sort(list);
+            return list[list.Length - 1];
+        }
+    }
+}
