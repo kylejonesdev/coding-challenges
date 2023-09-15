f07ddd6dd5902b3ef86914daf00659dfa0e8e2ca
Kyle Jones <kyle@kylejones.dev>
2023-09-15 10:55:35 -0700

    Double int; split string to array.

---
+++ codewars/CSharp/8kyu/20230915doubleInt/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _20230915doubleInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleInteger(2));
+            Console.WriteLine(StringToArray("Bob Bobbertson")[0]);
+        }
+        public static int DoubleInteger(int n)
+        {
+            return n * 2;
+        }
+        public static string[] StringToArray(string str)
+        {
+            return str.Split(' ');
+        }
+    }
+}
